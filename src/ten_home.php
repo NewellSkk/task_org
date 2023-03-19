f2fa331575bb53506cd9753b857aaa1824ee76db
NewellSkk <newellsikuku@gmail.com>
2023-03-19 16:20:43 +0000

    .

--- src/ten_home.php
+++ src/ten_home.php
@@ -6,9 +6,44 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Home Page</title>
+    <link rel="stylesheet" href="sign-up.css">
+    <link rel="stylesheet" href="nav.css">
+    <link rel="stylesheet" href="index.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  
 </head>
 <body>
-    THIS IS THE HOME PAGE.
+    <?php
+     require'ten_nav.php';
+    ?>
+    <div class="container">
+        <!-- DISPLAY ACCOUNT DETAILS -->
+        <div class="box">
+            <div class="fl iconBox">
+                <i class="material-icons">list</i>
+            </div>
+            <label class="fl label"> Account Details </label>
+        </div>
+        <!--
+            DISPLAY DETAILS HERE 
+            <div>
+            <p>Name:</p>
+            <p>House Number:</p>
+            <p>Email:</p>
+        </div> 
+    -->
+
+        <!-- CHANGE PASSWORD -->
+        <div class="box">
+            <div class="fl iconBox">
+                <i class="material-icons">mode_edit</i>
+            </div>
+            <label class="fl label"> Change Password </label>
+        </div>
+        <!-- 
+            DISPLAY FORM HERE
+         -->
+    </div>    
 </body>
 </html>
---
+++ src/ten_nav.php
@@ -0,0 +1,22 @@
+<?php
+ session_start();
+ $building=$_SESSION['building'];
+ $house_no=$_SESSION['house_no'];
+?>
+    <div>
+       <div class='building'>
+          <?php
+           echo $building.' Tenant';
+          ?>
+       </div>
+ 
+      <nav>
+   
+        <ul class="menuItems">
+          <li><a href='ten_home.php' data-item='Home'>Home</a></li>
+          <li><a href='#' data-item='Maintenance'>Maintenance</a></li>
+          <li><a class='logout' href='ten_login.php' data-item='Log-out'>Log-out</a></li>
+        </ul>
+      </nav>
+ 
+    </div>
