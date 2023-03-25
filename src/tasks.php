9b8370112fb272b1a7a3fe6167b579dbd3685f07
NewellSkk <newellsikuku@gmail.com>
2023-03-25 23:53:08 +0000

    .

--- src/tasks.php
+++ src/tasks.php
@@ -3,6 +3,12 @@
    require'../connect.php';
    
    $building=$_SESSION['building'];
+   $sql="SELECT * FROM tasks WHERE building='$building';";
+   $result=mysqli_query($database,$sql);
+    // while($row=mysqli_fetch_assoc($result)){
+    //     $row
+    // }    
+   
   
    ?>
 <!DOCTYPE html>
@@ -20,29 +26,68 @@
     <?php 
     require'nav.php'
     ?>
-     <div class='container'>
+    <div class='container'>
             <div class="category">
-            <span class="active general">General</span>
-            <span class="electric">Electric</span>
-            <span class="plumbing">Plumbing</span>
+                <span class="active general">General</span>
+                <span class="electric">Electric</span>
+                <span class="plumbing">Plumbing</span>
             </div>
             
+        <form action="#" method="post">
+
             <div class="list show general" >
+                <input type="submit" value="DONE">
                 <?php
+                    function card($res){
+                        while($row=mysqli_fetch_assoc($res)){
+                            $current=date('Y-m-d H:i:s');
+                            $datetime1 = new DateTime($row['time']);//start time
+                            $datetime2 = new DateTime($current);//end time
+                            $interval = $datetime1->diff($datetime2);
+                            $days= $interval->format('%a days');
+                            $days=2;
+                            if($days<3){
+                                echo '<div class="card"><p>TASK ID:'.$row['id']
+                                .'</p><p>HOUSE NUMBER:'.$row['house_no']
+                                .'</p><p>TASK:'.$row['task']
+                                .'</p><p>EMAIL'.$row['email']
+                                .'</p><p>REPORTED:'.$days
+                                .' DAYS AGO</p><input type="radio" value="'.$row['id'].'" name="done">COMPLETE</div>';
+                            }else{
+                                echo '<div class="overdue"><div class="card"><p>TASK ID:'.$row['id']
+                                .'</p><p>HOUSE NUMBER:'.$row['house_no']
+                                .'</p><p>TASK:'.$row['task']
+                                .'</p><p>EMAIL'.$row['email']
+                                .'</p><p>REPORTED:'.$days
+                                .' DAYS AGO</p><input type="radio" value="'.$row['id'].'" name="done">COMPLETE</div></div>';
+                            }
+                         }
+                    } 
+
                      $sql="SELECT * FROM tasks WHERE (category='general'AND building='$building');";
                      $result=mysqli_query($database,$sql);
-                     while($row=mysqli_fetch_assoc($result)){
-                        echo '<p>'.$row['task'].' '.$row['house_no'].'</p>';
-                     }
+                     card($result);
+                     
                 ?>
             </div>    
             <div class="list plumbing">
-            
+                <?php
+                    $sql="SELECT * FROM tasks WHERE (category='plumbing'AND building='$building');";
+                    $result=mysqli_query($database,$sql);
+                    card($result);
+                ?>
             </div>
             <div class="list electric">
+                <?php
+                    $sql="SELECT * FROM tasks WHERE (category='electric'AND building='$building');";
+                    $result=mysqli_query($database,$sql);
+                    card($result);
+                ?>
 
             </div>
-     </div>
-    <script src="ten_maintenance.js"></script>
+         </form>
+
+    </div>
+<script src="ten_maintenance.js"></script>
 </body>
 </html>
